32448eeee15746c38a25997961ccf0d92c493c01
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-01-27 15:06:01 -0300

    config clodinaryy

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -5,7 +5,7 @@ cloudinary.config({
     api_key: process.env.CLAUDINARY_API_KEY,
     api_secret:process.env.CLAUDINARY_API_SECRET,
     api_url:process.env.CLOUDINARY_URL,   
-    api_preset:UPLOAD_PRESET
+    api_preset:process.env.UPLOAD_PRESET
 });
 
 module.exports= cloudinary;
