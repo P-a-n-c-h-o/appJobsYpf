173097f0c06bc906ba2d4fba8c30191202398592
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-01-30 13:24:36 -0300

    elimamndo git ignore

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,11 +1,9 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-    cloud_name: process.env.CLAUDINARY_USER,
+    cloud_name: process.env.CLAUDINARY_USER_NAME,
     api_key: process.env.CLAUDINARY_API_KEY,
-    api_secret:process.env.CLAUDINARY_API_SECRET,
-    api_url:process.env.CLOUDINARY_URL    
-
+    api_secret:process.env.CLAUDINARY_API_SECRET
 });
 
 module.exports= cloudinary;
