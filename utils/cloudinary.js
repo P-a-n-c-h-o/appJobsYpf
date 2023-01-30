1fbb9479aa79047e8df7b7cb80e11c6158a79e1b
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-01-30 10:55:30 -0300

    CLOUDINARY

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,11 +1,12 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-    cloud_name: process.env.CLAUDINARY_USER,
-    api_key: process.env.CLAUDINARY_API_KEY,
-    api_secret: process.env.CLAUDINARY_API_SECRET,
-    //api_url: process.env.CLOUDINARY_URL,   
-    api_preset: process.env.UPLOAD_PRESET
+    cloud_name: process.env.CLOUDINARY_USER,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_url: process.env.CLOUDINARY_URL,   
+    api_preset: process.env.UPLOAD_PRESET,
+    secure: true
 });
 
 module.exports= cloudinary;
